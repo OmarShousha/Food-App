0a1b89cd58c0f2ceca028a285c8ffec91ef1e6df
omarShousha <oshousha6@gmail.com>
2024-04-25 06:21:14 +0200

    u

--- src/Modules/SharedModules/Components/Notfound/Notfound.jsx
+++ src/Modules/SharedModules/Components/Notfound/Notfound.jsx
@@ -30,7 +30,12 @@ export default function Notfound() {
                   <p className='mb-4 mb-lg-5 ms-md-2'>
                     This Page doesn’t exist or was removed! <br /> We suggest you  back to home.
                   </p>
-                  <Link to='' className="btn btn-success w-50 ms-md-2">Back to <br /> Home</Link>
+                  <Link to='' className="btn btn-success w-50 ms-md-2 d-flex justify-content-center">
+                    <div className='d-flex justify-content-center align-items-center w-fit gap-3'>
+                      <i class="fa-solid fs- fa-arrow-left-long bg-warnin"></i>
+                      <p className='m-0 me-3'>Back to <br /> Home</p>
+                    </div>
+                  </Link>
               </div>
 
           </div>
